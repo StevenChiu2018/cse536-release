7bfa4d93077291cbd7a9dcb89519efa7e4857128
StevenChiu2018 <sam.200846@gmail.com>
2023-09-07 17:59:35 -0700

    q_2_6

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -11,12 +11,24 @@
 struct elfhdr* kernel_elfhdr;
 struct proghdr* kernel_phdr;
 
+uchar* get_binary_addr(enum kernel ktype)
+{
+    if(ktype == NORMAL) return RAMDISK;
+    else return RECOVERYDISK;
+}
+
+struct elfhdr* get_elf_header(enum kernel ktype)
+{
+    return (struct elfhdr*)get_binary_addr(ktype)
+}
+
 uint64 find_kernel_load_addr(enum kernel ktype) {
     /* CSE 536: Get kernel load address from headers */
-    kernel_elfhdr = (struct elfhdr*)RAMDISK;
+    binary_addr = get_binary_addr(ktype)
+    kernel_elfhdr = get_elf_header(ktype);
     uint64 offset = kernel_elfhdr -> phoff;
     ushort size = kernel_elfhdr -> phentsize;
-    uint64 text_address = RAMDISK + offset + size;
+    uint64 text_address = binary_addr + offset + size;
     kernel_phdr = (struct proghdr*)text_address;
 
     return kernel_phdr -> vaddr;
@@ -24,14 +36,14 @@ uint64 find_kernel_load_addr(enum kernel ktype) {
 
 uint64 find_kernel_size(enum kernel ktype) {
     /* CSE 536: Get kernel binary size from headers */
-    kernel_elfhdr = (struct elfhdr*)RAMDISK;
+    kernel_elfhdr = get_elf_header(ktype);
 
     return kernel_elfhdr -> shoff + (kernel_elfhdr -> shentsize * kernel_elfhdr -> shnum);
 }
 
 uint64 find_kernel_entry_addr(enum kernel ktype) {
     /* CSE 536: Get kernel entry point from headers */
-    kernel_elfhdr = (struct elfhdr*)RAMDISK;
+    kernel_elfhdr = get_elf_header(ktype);
 
     return kernel_elfhdr -> entry;
 }
--- xv6-riscv/bootloader/start.c
+++ xv6-riscv/bootloader/start.c
@@ -39,33 +39,7 @@ void panic(char *s)
     ;
 }
 
-/* CSE 536: Boot into the RECOVERY kernel instead of NORMAL kernel
- * when hash verification fails. */
-void setup_recovery_kernel(void) {
-}
-
-/* CSE 536: Function verifies if NORMAL kernel is expected or tampered. */
-bool is_secure_boot(void) {
-  bool verification = true;
-
-  /* Read the binary and update the observed measurement
-   * (simplified template provided below) */
-  sha256_init(&sha256_ctx);
-  struct buf b;
-  sha256_update(&sha256_ctx, (const unsigned char*) b.data, BSIZE);
-  sha256_final(&sha256_ctx, sys_info_ptr->observed_kernel_measurement);
-
-  /* Three more tasks required below:
-   *  1. Compare observed measurement with expected hash
-   *  2. Setup the recovery kernel if comparison fails
-   *  3. Copy expected kernel hash to the system information table */
-  if (!verification)
-    setup_recovery_kernel();
-
-  return verification;
-}
-
-void copy_kernel_to(uint64 destination, uint64 size)
+void copy_kernel_to(uint64 destination, uint64 size, enum kernel ktype)
 {
   int blockno = 0;
   int blockno_offset = 4;
@@ -74,13 +48,59 @@ void copy_kernel_to(uint64 destination, uint64 size)
   while(copied_size < size)
   {
     buffer.blockno = blockno + blockno_offset;
-    kernel_copy(NORMAL, &buffer);
+    kernel_copy(ktype, &buffer);
     uint64 copy_to_addr = destination + (blockno * BSIZE);
     memmove((char *)copy_to_addr, buffer.data, BSIZE);
 
     blockno++;
     copied_size += BSIZE;
   }
+}
+
+/* CSE 536: Boot into the RECOVERY kernel instead of NORMAL kernel
+ * when hash verification fails. */
+void setup_kernel(enum kernel ktype)
+{
+  uint64 kernel_load_addr = find_kernel_load_addr(ktype);
+  uint64 kernel_binary_size = find_kernel_size(ktype);
+  copy_kernel_to(kernel_load_addr, kernel_binary_size, ktype);
+  uint64 kernel_entry = find_kernel_entry_addr(ktype);
+
+  /* CSE 536: Write the correct kernel entry point */
+  w_mepc((uint64) kernel_entry);
+}
+
+bool verified_kernel_hash()
+{
+  for(int i=0;i<32;i++)
+    if(sys_info_ptr -> expected_kernel_measurement[i] != sys_info_ptr -> observed_kernel_measurement[i])
+      return false;
+
+  return true;
+}
+
+void load_expected_kernel_measurement()
+{
+  for(int i=0;i<32;i++)
+    sys_info_ptr -> expected_kernel_measurement[i] = trusted_kernel_hash[i];
+}
+
+/* CSE 536: Function verifies if NORMAL kernel is expected or tampered. */
+bool is_secure_boot(void) {
+  /* Read the binary and update the observed measurement
+   * (simplified template provided below)
+   * Three more tasks required below:
+   *  1. Compare observed measurement with expected hash
+   *  2. Setup the recovery kernel if comparison fails
+   *  3. Copy expected kernel hash to the system information table */
+
+  sha256_init(&sha256_ctx);
+  uint binary_size = find_kernel_size(NORMAL);
+  sha256_update(&sha256_ctx, (uchar*) RAMDISK, binary_size);
+  sha256_final(&sha256_ctx, sys_info_ptr->observed_kernel_measurement);
+  load_expected_kernel_measurement();
+
+  return verified_kernel_hash();
 }
 
 // entry.S jumps here in machine mode on stack0.
@@ -126,22 +146,9 @@ void start()
   #endif
 
   /* CSE 536: Verify if the kernel is untampered for secure boot */
-  if (!is_secure_boot()) {
-    /* Skip loading since we should have booted into a recovery kernel
-     * in the function is_secure_boot() */
-    goto out;
-  }
+  if (is_secure_boot()) setup_kernel(NORMAL);
+  else setup_kernel(RECOVERY);
 
-  /* CSE 536: Load the NORMAL kernel binary (assuming secure boot passed). */
-  uint64 kernel_load_addr       = find_kernel_load_addr(NORMAL);
-  uint64 kernel_binary_size     = find_kernel_size(NORMAL);
-  copy_kernel_to(kernel_load_addr, kernel_binary_size);
-  uint64 kernel_entry           = find_kernel_entry_addr(NORMAL);
-
-  /* CSE 536: Write the correct kernel entry point */
-  w_mepc((uint64) kernel_entry);
-
- out:
   /* CSE 536: Provide system information to the kernel. */
 
   /* CSE 536: Send the observed hash value to the kernel (using sys_info_ptr) */
