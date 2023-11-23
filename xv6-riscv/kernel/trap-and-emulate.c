412a413201e8275c2ef1836d148a7ee9b99d03ed
StevenChiu2018 <sam.200846@gmail.com>
2023-11-22 18:36:53 -0700

    q_3 Get trap instruction

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -27,6 +27,16 @@ struct vm_virtual_state {
     struct vm_reg tmp;
 };
 
+struct instruct {
+    uint32 op;
+    uint32 rd;
+    uint32 funct3;
+    uint32 rs1;
+    uint32 uimm;
+};
+
+struct instruct get_trap_instruction(void);
+
 // In your ECALL, add the following for prints
 // struct proc* p = myproc();
 // printf("(EC at %p)\n", p->trapframe->epc);
@@ -35,16 +45,45 @@ void trap_and_emulate(void) {
     /* Comes here when a VM tries to execute a supervisor instruction. */
 
     /* Retrieve all required values from the instruction */
-    uint64 addr     = 0;
-    uint32 op       = 0;
-    uint32 rd       = 0;
-    uint32 funct3   = 0;
-    uint32 rs1      = 0;
-    uint32 uimm     = 0;
+    uint64 addr     = r_sepc();
+    struct instruct trap_instruct = get_trap_instruction();
 
     /* Print the statement */
-    printf("(PI at %p) op = %x, rd = %x, funct3 = %x, rs1 = %x, uimm = %x\n", 
-                addr, op, rd, funct3, rs1, uimm);
+    printf("(PI at %p) op = %x, rd = %x, funct3 = %x, rs1 = %x, uimm = %x\n",
+                addr, trap_instruct.op, trap_instruct.rd, trap_instruct.funct3, trap_instruct.rs1, trap_instruct.uimm);
+}
+
+uint32 extract_instruction(void);
+struct instruct decode_instruction(uint32 instruction);
+
+struct instruct get_trap_instruction(void) {
+    uint64 coded_instruction = extract_instruction();
+    return decode_instruction(coded_instruction);
+}
+
+uint32 extract_instruction(void) {
+    struct proc *p = myproc();
+    uint64 instruction_va = r_sepc();
+    char *buf = kalloc();
+    copyin(p->pagetable, buf, instruction_va, PGSIZE);
+
+    return *((uint32*) buf);
+}
+
+struct instruct decode_instruction(uint32 coded_instruction) {
+    struct instruct instruction;
+
+    instruction.op = coded_instruction % 128;
+    coded_instruction >>= 7;
+    instruction.rd = coded_instruction % 32;
+    coded_instruction >>= 5;
+    instruction.funct3 = coded_instruction % 8;
+    coded_instruction >>= 3;
+    instruction.rs1 = coded_instruction % 32;
+    coded_instruction >>= 5;
+    instruction.uimm = coded_instruction;
+
+    return instruction;
 }
 
 void trap_and_emulate_init(void) {
