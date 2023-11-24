69554b0bd4e600d29182682a13a5df6eefdd6aca
StevenChiu2018 <sam.200846@gmail.com>
2023-11-24 02:15:42 -0700

    rw to registers

---
+++ xv6-riscv/kernel/rw_registers.h
@@ -0,0 +1,126 @@
+#include "riscv.h"
+#include "types.h"
+
+void write_to_register(uint64 regis_code, uint64 value) {
+    switch (regis_code)
+    {
+    case 0x1:
+        w_ra(value);
+        break;
+
+    case 0x2:
+        w_sp(value);
+        break;
+
+    case 0x5:
+        w_t0(value);
+        break;
+
+    case 0x6:
+        w_t1(value);
+        break;
+
+    case 0x7:
+        w_t2(value);
+        break;
+
+    case 0x8:
+        w_s0(value);
+        break;
+
+    case 0x9:
+        w_s1(value);
+        break;
+
+    case 0xa:
+        w_a0(value);
+        break;
+
+    case 0xb:
+        w_a1(value);
+        break;
+
+    case 0xc:
+        w_a2(value);
+        break;
+
+    case 0xd:
+        w_a3(value);
+        break;
+
+    case 0xe:
+        w_a4(value);
+        break;
+
+    case 0xf:
+        w_a5(value);
+        break;
+
+    case 0x10:
+        w_a6(value);
+        break;
+
+    case 0x11:
+        w_a7(value);
+        break;
+
+    case 0x12:
+        w_s2(value);
+        break;
+
+    case 0x13:
+        w_s3(value);
+        break;
+
+    case 0x14:
+        w_s4(value);
+        break;
+
+    case 0x15:
+        w_s5(value);
+        break;
+
+    case 0x16:
+        w_s6(value);
+        break;
+
+    case 0x17:
+        w_s7(value);
+        break;
+
+    case 0x18:
+        w_s8(value);
+        break;
+
+    case 0x19:
+        w_s9(value);
+        break;
+
+    case 0x1a:
+        w_s10(value);
+        break;
+
+    case 0x1b:
+        w_s11(value);
+        break;
+
+    case 0x1c:
+        w_t3(value);
+        break;
+
+    case 0x1d:
+        w_t4(value);
+        break;
+
+    case 0x1e:
+        w_t5(value);
+        break;
+
+    case 0x1f:
+        w_t6(value);
+        break;
+
+    default:
+        break;
+    }
+}
--- xv6-riscv/kernel/vm_state.h
+++ xv6-riscv/kernel/vm_state.h
@@ -1,68 +1,80 @@
 #include "types.h"
 
-enum execution_mode {MACHINE, SUPERVISOR, USER};
+#define PRIVI_REGS_AMOUNT 33
+
+enum execution_mode {USER, SUPERVISOR, MACHINE};
 
 // Struct to keep VM registers (Sample; feel free to change.)
 struct vm_reg {
     int code;
-    enum execution_mode mode;
     uint64 val;
+    uint32 auth; // the leftmost digit present the auth of read, and the rightmost digit presetn the auth of write
 };
 
 // Keep the virtual state of the VM's privileged registers
 struct vm_state {
-    struct vm_reg privi_regs[33];
+    struct vm_reg privi_regs[PRIVI_REGS_AMOUNT];
 };
 
-void setup_regs(struct vm_state*);
+void setup_privi_regs(struct vm_state*);
 
 struct vm_state generate_vm_state(void) {
     struct vm_state state;
 
-    setup_regs(&state);
+    setup_privi_regs(&state);
 
     return state;
 }
 
-void setup_regs(struct vm_state *state) {
+void setup_privi_regs(struct vm_state *state) {
     // Machine trap handling registers
-    state->privi_regs[0] = (struct vm_reg){.code = 0x340, .mode = MACHINE, .val = 0};
-    state->privi_regs[1] = (struct vm_reg){.code = 0x341, .mode = MACHINE, .val = 0};
-    state->privi_regs[2] = (struct vm_reg){.code = 0x342, .mode = MACHINE, .val = 0};
-    state->privi_regs[3] = (struct vm_reg){.code = 0x343, .mode = MACHINE, .val = 0};
-    state->privi_regs[4] = (struct vm_reg){.code = 0x344, .mode = MACHINE, .val = 0};
-    state->privi_regs[5] = (struct vm_reg){.code = 0x34a, .mode = MACHINE, .val = 0};
-    state->privi_regs[6] = (struct vm_reg){.code = 0x34b, .mode = MACHINE, .val = 0};
+    state->privi_regs[0] = (struct vm_reg){.code = 0x340, .auth = 0x22, .val = 0};
+    state->privi_regs[1] = (struct vm_reg){.code = 0x341, .auth = 0x22, .val = 0};
+    state->privi_regs[2] = (struct vm_reg){.code = 0x342, .auth = 0x22, .val = 0};
+    state->privi_regs[3] = (struct vm_reg){.code = 0x343, .auth = 0x22, .val = 0};
+    state->privi_regs[4] = (struct vm_reg){.code = 0x344, .auth = 0x22, .val = 0};
+    state->privi_regs[5] = (struct vm_reg){.code = 0x34a, .auth = 0x22, .val = 0};
+    state->privi_regs[6] = (struct vm_reg){.code = 0x34b, .auth = 0x22, .val = 0};
     // Machine trap setup registers
-    state->privi_regs[7] = (struct vm_reg){.code = 0x300, .mode = MACHINE, .val = 0};
-    state->privi_regs[8] = (struct vm_reg){.code = 0x301, .mode = MACHINE, .val = 0};
-    state->privi_regs[9] = (struct vm_reg){.code = 0x302, .mode = MACHINE, .val = 0};
-    state->privi_regs[10] = (struct vm_reg){.code = 0x303, .mode = MACHINE, .val = 0};
-    state->privi_regs[11] = (struct vm_reg){.code = 0x304, .mode = MACHINE, .val = 0};
-    state->privi_regs[12] = (struct vm_reg){.code = 0x305, .mode = MACHINE, .val = 0};
-    state->privi_regs[13] = (struct vm_reg){.code = 0x306, .mode = MACHINE, .val = 0};
-    state->privi_regs[14] = (struct vm_reg){.code = 0x310, .mode = MACHINE, .val = 0};
+    state->privi_regs[7] = (struct vm_reg){.code = 0x300, .auth = 0x22, .val = 0};
+    state->privi_regs[8] = (struct vm_reg){.code = 0xf10, .auth = 0x22, .val = 0};
+    state->privi_regs[9] = (struct vm_reg){.code = 0x302, .auth = 0x22, .val = 0};
+    state->privi_regs[10] = (struct vm_reg){.code = 0x303, .auth = 0x22, .val = 0};
+    state->privi_regs[11] = (struct vm_reg){.code = 0x304, .auth = 0x22, .val = 0};
+    state->privi_regs[12] = (struct vm_reg){.code = 0x305, .auth = 0x22, .val = 0};
+    state->privi_regs[13] = (struct vm_reg){.code = 0x306, .auth = 0x22, .val = 0};
+    state->privi_regs[14] = (struct vm_reg){.code = 0x310, .auth = 0x22, .val = 0};
     // Machine information registers
-    state->privi_regs[15] = (struct vm_reg){.code = 0xf11, .mode = MACHINE, .val = 0x637365353336};
-    state->privi_regs[16] = (struct vm_reg){.code = 0xf12, .mode = MACHINE, .val = 0};
-    state->privi_regs[17] = (struct vm_reg){.code = 0xf13, .mode = MACHINE, .val = 0};
-    state->privi_regs[18] = (struct vm_reg){.code = 0xf14, .mode = MACHINE, .val = 0};
-    state->privi_regs[19] = (struct vm_reg){.code = 0xf15, .mode = MACHINE, .val = 0};
+    state->privi_regs[15] = (struct vm_reg){.code = 0xf11, .auth = 0x02, .val = 0x637365353336};
+    state->privi_regs[16] = (struct vm_reg){.code = 0xf12, .auth = 0x22, .val = 0};
+    state->privi_regs[17] = (struct vm_reg){.code = 0xf13, .auth = 0x22, .val = 0};
+    state->privi_regs[18] = (struct vm_reg){.code = 0xf14, .auth = 0x22, .val = 0};
+    state->privi_regs[19] = (struct vm_reg){.code = 0xf15, .auth = 0x22, .val = 0};
     // Supervisor page table register
-    state->privi_regs[20] = (struct vm_reg){.code = 0x180, .mode = SUPERVISOR, .val = 0};
+    state->privi_regs[20] = (struct vm_reg){.code = 0x180, .auth = 0x11, .val = 0};
     // Supervisor trap setup registers
-    state->privi_regs[21] = (struct vm_reg){.code = 0x100, .mode = SUPERVISOR, .val = 0};
-    state->privi_regs[22] = (struct vm_reg){.code = 0x104, .mode = SUPERVISOR, .val = 0};
-    state->privi_regs[23] = (struct vm_reg){.code = 0x105, .mode = SUPERVISOR, .val = 0};
-    state->privi_regs[24] = (struct vm_reg){.code = 0x106, .mode = SUPERVISOR, .val = 0};
+    state->privi_regs[21] = (struct vm_reg){.code = 0x100, .auth = 0x11, .val = 0};
+    state->privi_regs[22] = (struct vm_reg){.code = 0x104, .auth = 0x11, .val = 0};
+    state->privi_regs[23] = (struct vm_reg){.code = 0x105, .auth = 0x11, .val = 0};
+    state->privi_regs[24] = (struct vm_reg){.code = 0x106, .auth = 0x11, .val = 0};
     // User trap handling registers
-    state->privi_regs[25] = (struct vm_reg){.code = 0x040, .mode = USER, .val = 0};
-    state->privi_regs[26] = (struct vm_reg){.code = 0x041, .mode = USER, .val = 0};
-    state->privi_regs[27] = (struct vm_reg){.code = 0x042, .mode = USER, .val = 0};
-    state->privi_regs[28] = (struct vm_reg){.code = 0x043, .mode = USER, .val = 0};
-    state->privi_regs[29] = (struct vm_reg){.code = 0x044, .mode = USER, .val = 0};
+    state->privi_regs[25] = (struct vm_reg){.code = 0x040, .auth = 0x00, .val = 0};
+    state->privi_regs[26] = (struct vm_reg){.code = 0x041, .auth = 0x00, .val = 0};
+    state->privi_regs[27] = (struct vm_reg){.code = 0x042, .auth = 0x00, .val = 0};
+    state->privi_regs[28] = (struct vm_reg){.code = 0x043, .auth = 0x00, .val = 0};
+    state->privi_regs[29] = (struct vm_reg){.code = 0x044, .auth = 0x00, .val = 0};
     // User trap setup registers
-    state->privi_regs[30] = (struct vm_reg){.code = 0x000, .mode = USER, .val = 0};
-    state->privi_regs[31] = (struct vm_reg){.code = 0x004, .mode = USER, .val = 0};
-    state->privi_regs[32] = (struct vm_reg){.code = 0x005, .mode = USER, .val = 0};
+    state->privi_regs[30] = (struct vm_reg){.code = 0x000, .auth = 0x00, .val = 0};
+    state->privi_regs[31] = (struct vm_reg){.code = 0x004, .auth = 0x00, .val = 0};
+    state->privi_regs[32] = (struct vm_reg){.code = 0x005, .auth = 0x00, .val = 0};
+}
+
+struct vm_reg* get_privi_reg(struct vm_state* state, int code) {
+    for(int i = 0;i < PRIVI_REGS_AMOUNT;i++) {
+        if(state->privi_regs[i].code == code) {
+            return &state->privi_regs[i];
+        }
+    }
+
+    return NULL;
 }
